80ecc1d6ed495d6cd8619c57643ae3c7ce59d178
ABDOU KARIM GAYE <36362851+Bocoul@users.noreply.github.com>
2020-08-13 17:16:42 +0200

    clean

--- ctrlm/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from ctrlm.models import Pdl, Etab, Liste
-
-admin.site.register(Pdl)
-admin.site.register(Etab)
-admin.site.register(Liste)
--- ctrlm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CtrlmConfig(AppConfig):
-    name = 'ctrlm'
